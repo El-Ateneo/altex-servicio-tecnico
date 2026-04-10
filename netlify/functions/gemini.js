cdd48322b5ab014cb276ac8e3dc8af309c783eaa
El-Ateneo <126694108+El-Ateneo@users.noreply.github.com>
2026-04-09 22:50:38 -0300

    Add files via upload

---
+++ netlify/functions/gemini.js
@@ -0,0 +1,37 @@
+export async function handler(event) {
+  const API_KEY = process.env.GEMINI_API_KEY;
+
+  try {
+    const { prompt } = JSON.parse(event.body);
+
+    const response = await fetch(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-09-2025:generateContent?key=${API_KEY}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          contents: [
+            {
+              parts: [{ text: prompt }]
+            }
+          ]
+        })
+      }
+    );
+
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Error en servidor" })
+    };
+  }
+}
