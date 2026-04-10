2443744568858a902adf6a37b4c3d244981ec539
El-Ateneo <126694108+El-Ateneo@users.noreply.github.com>
2026-04-09 23:26:01 -0300

    Update gemini.js

--- netlify/functions/gemini.js
+++ netlify/functions/gemini.js
@@ -5,7 +5,7 @@ export async function handler(event) {
     const { prompt } = JSON.parse(event.body);
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-09-2025:generateContent?key=${API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
       {
         method: "POST",
         headers: {
